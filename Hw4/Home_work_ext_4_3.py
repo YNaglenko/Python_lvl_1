be384d5f125baac6f33a0b4e0ad657236ce665c2
naglenko.ig <naglenko.ig@gmail.com>
2021-11-21 10:19:39 +0200

    added task 4_3 extended v2

--- Hw4/Home_work_ext_4_3.py
+++ Hw4/Home_work_ext_4_3.py
@@ -6,20 +6,43 @@
 #  ***
 # *****
 
+
 w = int(input("Enter width: "))
-if w % 2 != 0:
-    ws = 0
-    st = w
+q_steps = (w // 2) + 1  # кількість ітерацій циклу
 
-    while st > 1:
+if w % 2 != 0:  # Якщо число не парне
+    ws = 0      # пробіли
+    st = w      # символи
+    revers = 1  # коеф розвороту
+    i = q_steps
+
+    while i >= 0:
         print(ws * ' ', st * '*', ws * ' ')
-        ws = ws + 1
-        st = st - 2
-    else:
-        while st <= w:
-            print(ws * ' ', st * '*', ws * ' ')
-            ws = ws - 1
-            st = st + 2
-
+        ws = ws + 1 * revers
+        st = st - 2 * revers
+        if st == 1:  # закінчення циклу зміюємо коеф. на -1
+            revers = -1
+            i = q_steps  # оновлюємо кількість ітераці
+        i = i - 1
 else:
     print("Number is even")
+
+# Variant 2
+
+# w = int(input("Enter width: "))
+# if w % 2 != 0:
+#     ws = 0
+#     st = w
+#
+#     while st > 1:
+#         print(ws * ' ', st * '*', ws * ' ')
+#         ws = ws + 1
+#         st = st - 2
+#     else:
+#         while st <= w:
+#             print(ws * ' ', st * '*', ws * ' ')
+#             ws = ws - 1
+#             st = st + 2
+#
+# else:
+#     print("Number is even")
