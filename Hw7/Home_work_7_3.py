c86b48dcdd34a913e010c1d81980c32bd0b47f13
naglenko.ig <naglenko.ig@gmail.com>
2021-11-25 00:04:17 +0200

    add homework 7_3

--- Hw7/Home_work_7_3.py
+++ Hw7/Home_work_7_3.py
@@ -5,11 +5,11 @@
 txt_string = input("Enter a txt string: ")
 txt_parts = {}
 for letter in txt_string:
-    qnty = txt_parts.get(letter)
-    if qnty is None:
+    count = txt_parts.get(letter)
+    if count is None:
         txt_parts[letter] = 1
     else:
-        txt_parts[letter] = qnty + 1
+        txt_parts[letter] = count + 1
 
 for key in txt_parts.keys():
     print(key, '-', txt_parts[key])
