b215256fa3166629fdfab78e33ecf6b8a979fb32
naglenko.ig <naglenko.ig@gmail.com>
2021-11-24 23:11:46 +0200

    add homework 7_2

---
+++ Hw7/Home_work_7_2.py
@@ -0,0 +1,26 @@
+# 2)Представьте описание кота (домашнее животное) на основе словаря.
+
+cat = {"name": "Soniya",
+       "breed": "manx",
+       "age": 2,
+       "height": 20,
+       "weight": 4.5,
+       "colour": "grey",
+       "Address": {
+           "city": "Kyiv",
+           "street": "Boryspilska",
+           "house": 14,
+           "flat": 44,
+           "phone": "380444568260",
+           "owner": "Yevgen N"
+       },
+       "visitVetDate": {
+           "last": "2021-09-11",
+           "next": "2022-02-28"},
+       "vaccines": {
+           "Vacc_1": "Y",
+           "Vacc_2": "Y",
+           "Vacc_3": "No"
+       }
+
+       }
